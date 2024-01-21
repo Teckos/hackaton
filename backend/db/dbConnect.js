3296361673854cd5a6c0ecf4e23667b3789df00a
Teckos <teckos@hotmail.fr>
2024-01-21 18:19:57 +0100

    DB credentials are now stored in .env

--- backend/db/dbConnect.js
+++ backend/db/dbConnect.js
@@ -2,11 +2,11 @@ const mysql = require('mysql2/promise');
 exports.myCon = async ()=>{
 
     const con = await mysql.createConnection({
-        host: "db",
-        user: "user",
-        password: "secret-pw",
-        database: "hackaton",
-        port: 3306
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        port: process.env.DB_PORT
     });
     return con;
 };
